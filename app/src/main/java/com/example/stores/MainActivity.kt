893734a6d026b8d218b6f0ae25644e80d0b69407
josue recoba <josue.huer.reco.22@gamil.com>
2021-12-08 22:23:13 -0600

    RecyclerView configuracion

--- app/src/main/java/com/example/stores/MainActivity.kt
+++ app/src/main/java/com/example/stores/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.stores
 
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.stores.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+     private lateinit var mAdapter: StoreAdapter
+     private lateinit var mGridLayout:GradientDrawable
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupRecylcerView()
+    }
+
+    private fun setupRecylcerView() {
+        mAdapter = StoreAdapter(mutableListOf(), this)
+        mGridLayout = GridLayoutManager(this, 2 )
+
+        mBinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = mGridLayout
+            adapter = mAdapter
+        }
+
+    }
+    /*
+    * OnclickListener
+    **/
+
+    override fun onclick(store: Store) {
+
     }
 }
