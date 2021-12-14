f21b8e07fc4865b66c8e020bf69d15a09224ff9b
josue recoba <josue.huer.reco.22@gamil.com>
2021-12-13 21:45:56 -0600

    mejores practicas

--- app/src/main/java/com/cursosant/android/stores/EditStoreFragment.kt
+++ app/src/main/java/com/cursosant/android/stores/EditStoreFragment.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.os.Bundle
 import android.view.*
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.cursosant.android.stores.databinding.FragmentEditStoreBinding
-import com.google.android.material.snackbar.Snackbar
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -30,6 +33,14 @@ class EditStoreFragment : Fragment() {
         mActivity?.supportActionBar?.title = getString(R.string.edit_store_title_add)
 
         setHasOptionsMenu(true)
+
+        mBinding.etPhotoUrl.addTextChangedListener {
+            Glide.with(this)
+                .load(mBinding.etPhotoUrl.text.toString())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .into(mBinding.imgPhoto)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -49,16 +60,13 @@ class EditStoreFragment : Fragment() {
                         website = mBinding.etWebsite.text.toString().trim())
 
                 doAsync {
-                    StoreApplication.database.storeDao().addStore(store)
+                    store.id = StoreApplication.database.storeDao().addStore(store)
                     uiThread {
                         mActivity?.addStore(store)
 
                         hideKeyboard()
 
-                        Snackbar.make(mBinding.root,
-                            getString(R.string.edit_store_message_save_success),
-                            Snackbar.LENGTH_SHORT)
-                            .show()
+                        Toast.makeText(mActivity, R.string.edit_store_message_save_success, Toast.LENGTH_SHORT).show()
 
                         mActivity?.onBackPressed()
                     }
